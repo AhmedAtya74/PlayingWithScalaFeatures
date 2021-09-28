be2a056a83ae2db75973c76f2eae6a32d50d26f4
Ahmed Atya <64374947+AhmedAtya74@users.noreply.github.com>
2021-09-28 16:43:56 +0200

    Create operators.scala

---
+++ Operators/operators.scala
@@ -0,0 +1,31 @@
+package Demo
+
+import scala.io.StdIn._
+
+object Demo {
+
+
+  def main(args: Array[String]): Unit = {
+
+    // operators {+, -, *, /, %}
+
+    val numX = 5
+    val numY = 9
+
+    val add = numX + numY
+    println(add)
+
+    val sub = numY - numX
+    println(sub)
+
+    val multi = numY * numX
+    println(multi)
+
+    val mod = numY % numX
+    println(mod)
+    
+    
+
+  }
+
+}
